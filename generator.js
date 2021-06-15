84fdaee0d23d5e547d7073c8de7c453042963831
AmalaLiza <71072541+amala-james-cko@users.noreply.github.com>
2021-06-15 17:39:00 +0400

    Fixed issue with listing all documents

--- generator.js
+++ generator.js
@@ -4,6 +4,13 @@ const Prismic = require("prismic-javascript");
 const { SitemapStream, streamToPromise } = require('sitemap');
 
 /**
+ * Resolves promise.
+ */
+const resolvePromise = () => {
+  return Promise.resolve("ok");
+};
+
+/**
  * Generates a sitemap
  * @param {Object} sitemap The Sitemap Object
  * @param {Function} sitemap.linkResolver
@@ -14,70 +21,94 @@ const { SitemapStream, streamToPromise } = require('sitemap');
  * @param {Array} sitemap.documentTypes
  * @param {Object} sitemap.sitemapConfig
  */
-const generator = async (sitemap) => {
 
+const generator = async (sitemap) => {
   const {
     linkResolver = null /* doc => { return something } */,
-    apiEndpoint = '',
+    apiEndpoint = "",
     accessToken = null,
-    hostname = '',
+    hostname = "",
     optionsMapPerDocumentType = {},
-    documentTypes = ['*'],
-    fileName = 'sitemap.xml',
-    publicPath = 'public',
-    sitemapConfig
+    documentTypes = ["*"],
+    fileName = "sitemap.xml",
+    publicPath = "public",
+    sitemapConfig,
   } = sitemap;
 
-  if (typeof linkResolver !== 'function') {
-    throw new Error('The linkResolver function is undefined, this is needed to build sitemap links');
+  if (typeof linkResolver !== "function") {
+    throw new Error(
+      "The linkResolver function is undefined, this is needed to build sitemap links"
+    );
   }
 
   if (accessToken !== null && accessToken.length <= 1) {
-    throw new Error('The API Access token appears incorrect, please double check as it is short.');
+    throw new Error(
+      "The API Access token appears incorrect, please double check as it is short."
+    );
   }
 
   /** @todo Add extended options to the Prismic API function, or to pass one from user level */
-  const api = await Prismic.getApi(apiEndpoint, { 'accessToken': accessToken });
+  const api = await Prismic.getApi(apiEndpoint, { accessToken: accessToken });
 
-  const { results: docs } = await api.query(
-    Prismic.Predicates.any('document.type', documentTypes),
-    {
-      lang: "*"
+  let docs = [];
+
+  /**
+   * gets all documents of type doc and pushes it to docs.
+   * @param {*} doc
+   */
+  const populatePaths = async (doc) => {
+    const { results } = await api.query(
+      Prismic.Predicates.at("document.type", doc),
+      {
+        lang: "*",
+        pageSize: 10000, // to get all pages
+      }
+    );
+    docs.push(...results);
+    return resolvePromise(doc);
+  };
+
+  const getData = async () => {
+    return Promise.all(documentTypes.map((item) => populatePaths(item)));
+  };
+
+  return getData().then(async () => {
+    const sitemapStream = new SitemapStream({
+      hostname: hostname,
+      ...sitemapConfig,
     });
 
-  const sitemapStream = new SitemapStream({ hostname: hostname, ...sitemapConfig });
+    docs
+      .sort((a, b) => (a.type < b.type ? -1 : 1)) // sort by type
+      .forEach((doc) => {
+        const options = optionsMapPerDocumentType.hasOwnProperty(doc.type)
+          ? optionsMapPerDocumentType[doc.type]
+          : { changefreq: "monthly", priority: 1 };
 
-  docs
-    .sort((a, b) => a.type < b.type ? -1 : 1) // sort by type
-    .forEach(doc => {
-
-      const options = optionsMapPerDocumentType.hasOwnProperty(doc.type)
-        ? optionsMapPerDocumentType[doc.type]
-        : { changefreq: "monthly", priority: 1 };
-
-      sitemapStream.write({
-        url: linkResolver(doc),
-        ...options
+        sitemapStream.write({
+          url: linkResolver(doc),
+          ...options,
+        });
       });
-    })
 
-  sitemapStream.end();
+    sitemapStream.end();
 
-  const sitemapData = await streamToPromise(sitemapStream);
+    const sitemapData = await streamToPromise(sitemapStream);
 
-  let basePath = resolvePublicPath(publicPath)
+    let basePath = resolvePublicPath(publicPath);
 
-  if (!fs.existsSync(path.join(basePath))) {
-    fs.mkdirSync(path.join(basePath), { recursive: true });
-  }
+    if (!fs.existsSync(path.join(basePath))) {
+      fs.mkdirSync(path.join(basePath), { recursive: true });
+    }
 
-  fs.writeFileSync(path.join(basePath, fileName), sitemapData, "utf-8");
+    fs.writeFileSync(path.join(basePath, fileName), sitemapData, "utf-8");
 
-  return sitemapData;
-}
+    return sitemapData;
+  });
+};
 
 function resolvePublicPath(dir) {
-  if (dir === 'public') {
+  if (dir === "public") {
     dir = path.join(__dirname, dir);
   }
 
