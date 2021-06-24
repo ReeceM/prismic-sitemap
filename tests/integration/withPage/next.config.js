c2b3672262c144b6776ea97e85591a03453e26c7
ReeceM <reecestevenmay@gmail.com>
2021-06-25 00:20:36 +0200

    update tests for v0.3.0-alpha.1

--- tests/integration/withPage/next.config.js
+++ tests/integration/withPage/next.config.js
@@ -3,16 +3,28 @@ const withPrismicSitemap = require('../../../index');
 module.exports = withPrismicSitemap({
   sitemap: {
     linkResolver: doc => {
-      return doc.type == 'post'
-        ? `blog/${doc.uid}`
-        : `page/${doc.uid}`;
+      switch (doc.type) {
+        case 'post':
+          return `blog/${doc.uid}`
+        case 'latest_notice':
+          return `notice/${doc.uid}`
+        default:
+          return `page/${doc.uid}`
+      }
     },
 
     apiEndpoint: process.env.TEST_REPOSITORY,
     hostname: 'https://example.com',
-    documentTypes: ['post', 'page'],
+    documentTypes: ['post', 'page', 'latest_notice'],
     optionsMapPerDocumentType: {
       post: { changefreq: "weekly", priority: 0.8 },
+      latest_notice: (doc) => {
+        return {
+          lastmod: doc.last_publication_date ? doc.last_publication_date : (new Date()).toJSON(),
+          changefreq: "weekly",
+          priority: 0.8
+        }
+      }
     },
   }
 })
